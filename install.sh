07d6ceb9c2e7261bde8bdc323844d14cbd661ec3
jrtuazon <46452487+jrtuazon@users.noreply.github.com>
2021-11-03 21:12:14 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 homedir=/home/ubuntu
 apppath="axieDailyTrackerBot-SSAxie"
+echo "Installing the requirements ... "
 
+echo "Updating source lists"
 sudo apt update
+
+echo "Installing python "
 sudo apt install -y python3-pip
 
+echo "Updating requirements file"
 cd $homedir/$apppath
 pip freeze > requirements.txt
 
+echo "Installing application requirements"
 pip3 install -r requirements.txt
 pip3 install discord
 pip3 install web3
